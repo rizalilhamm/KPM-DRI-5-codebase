cb99deb0d0327279f817214a036baacaa8042630
Rizal Ilham <rzlilhm09@gmail.com>
2022-03-21 21:41:25 +0700

    Commit Pertama

---
+++ hello.py
@@ -0,0 +1,23 @@
+# 17 Maret 2022: Menjalankan Program Hello World, Python Case Sensitivity
+# Case Sensitive: Adalah komputer membedakan antara kata yang memiliki  huruf BESAR dan huruf KECIL
+
+# print() == adalah yang digunakan untuk mencetak nilai, fungsi sering debugging (Mencari kesalahan dalam program)
+print("Hello, World!")
+
+namaLengkap = "Rizal Ilham"
+namalengkap = "Mardhatika"
+
+print("namaLengkap: ", namaLengkap)
+print("namalengkap: ", namalengkap)
+
+# Berbeda False dan True sama
+# print(namaLengkap == namalengkap)
+
+# Digunakan untuk memastikan bahwa dia tidak sama
+print(namaLengkap != namalengkap)
+
+
+"""
+Upper = Huruf besar
+Lower = Huruf Kecil
+"""
---
+++ hellow_world.py
@@ -0,0 +1,7 @@
+print("Hello, World!")
+print("Nama Saya Rizal Ilham")
+
+# Variable = digunakan untuk menampung data
+nama = input("Masukkan nama anda: ")
+
+print("Nama yang dimasukkan: ", nama)
