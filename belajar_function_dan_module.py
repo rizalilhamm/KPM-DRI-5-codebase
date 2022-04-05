883295c98b4ea5419d84f276a568aa758aea7dbd
Rizal Ilham <rzlilhm09@gmail.com>
2022-04-05 22:15:21 +0700

    Belajar tanggal  Maret

---
+++ belajar_function_dan_module.py
@@ -0,0 +1,34 @@
+# Function: Adalah blok kode terorganisir dan dapat digunakan kembali yang digunakan untuk melakukan sebuah tindakan/action. 
+#           Fungsi memberikan modularitas yang lebih baik untuk aplikasi Anda dan tingkat penggunaan kode yang tinggi.
+
+# Module:   Module pada Python adalah sebuah file yang berisikan sekumpulan kode fungsi, class dan variabel 
+#           yang disimpan dalam satu file berekstensi .py dan dapat dieksekusi oleh interpreter Python.
+
+# Import Statement: Module yang kita inport / panggil dari luar kode yang kita buat.
+
+def penjumlahan(a, b):
+    return a + b
+
+def pengurangan(a, b):
+    return a - b
+
+def perkalian(a, b):
+    return a * b
+
+def pembagian(a, b):
+    return a / b
+
+
+# Object
+a = 10
+b = 5
+
+# hasilpemjumlahan = penjumlahan(a, b)
+# hasilpengurangan = pengurangan(a, b)
+# hasilperkalian = perkalian(a, b)
+# hasilpembagian = pembagian(a, b)
+
+# print('HASIL PEMJUMLAHAN', hasilpemjumlahan)
+# print('HASIL PENGURANGAN', hasilpengurangan)
+# print('HASIL PERKALIAN', hasilperkalian)
+# print('HASIL PEMBAGIAN', hasilpembagian)
---
+++ penjumlahan_obj.py
@@ -0,0 +1,11 @@
+import belajar_function_dan_module
+
+from belajar_function_dan_module import penjumlahan
+
+hasil = penjumlahan(10, 3) # 13
+
+print(hasil)
+
+hasilkurang = belajar_function_dan_module.pengurangan(12, 8)
+
+print('hasil kurang', hasilkurang)
