6d1b5ba08732a20499ff6b7da57b4731ccd96f4d
Rizal Ilham <rzlilhm09@gmail.com>
2022-04-15 15:09:32 +0700

    08 April 2022

--- backend_services/app.py
+++ backend_services/app.py
@@ -1,10 +1,19 @@
-# Flask adalah micro framework web development
-# 1. Service kecil
-# 2. Biasa dipake untuk ngebuat API
-
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+# https://docs.python.org/3/library/os.path.html (Referensi belajar library os python)
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
+# kunci rahasia
+SECRET_KEY = os.getenv('SECRET_KEY') or 'ini-rahasia'
+
+# 
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
 
 @app.route('/')
 @app.route('/hello')
@@ -15,9 +24,12 @@ def hello_world():
 def profile():
     return 'Halaman Profile'
 
+# USER MODEL
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)    # render automaticlly by server if not inisilized
+    nama = db.Column(db.String(50), nullable=False, unique=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    bio = db.COlumn(db.String(150), nullable=True, unique=False)
 
-# 127.0.0.1:5000/
-# 127.0.0.1:5000/hello
-
-
-# 127.0.0.1:5000/profile
+    def __repr__(self):
+        return self.nama
